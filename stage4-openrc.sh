b9c5f4bfc1b7cb9f4e40d53d77e434765ad620b6
Luna Deards <toffee.1@icloud.com>
2023-01-30 13:58:44 +0000

    Fix sudo

--- stage4-openrc.sh
+++ stage4-openrc.sh
@@ -5,4 +5,5 @@ groupadd sudo
 usermod -aG sudo xenia
 echo "root:87658765XeniaLinux" | chpasswd
 echo "xenia:87658765XeniaLinux" | chpasswd
+chown root:root /etc/sudoers
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
