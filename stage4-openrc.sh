0207122a6018872c704ee15b4dcf77ad837a4026
Luna Deards <toffee.1@icloud.com>
2023-03-26 11:43:17 +0100

    change group from sudo to wheel

--- stage4-openrc.sh
+++ stage4-openrc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 dracut --force -a "lvm dmsquash-live"
 useradd xenia
-groupadd sudo
-usermod -aG sudo xenia
+groupadd wheel
+usermod -aG wheel xenia
 echo "root:87658765XeniaLinux" | chpasswd
 echo "xenia:87658765XeniaLinux" | chpasswd
 chown root:root /etc/sudoers
