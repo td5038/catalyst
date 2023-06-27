e094fce7ca66aed84f8b42cbcefed387db6fde0e
Luna Deards <toffee.1@icloud.com>
2023-06-27 02:27:52 +0100

    remove old kernels and such, prevent no kernel

--- stage4-openrc.sh
+++ stage4-openrc.sh
@@ -8,6 +8,8 @@ echo "root:87658765XeniaLinux" | chpasswd
 echo "xenia:87658765XeniaLinux" | chpasswd
 chown root:root /etc/sudoers
 
+rm /boot/*.old
+
 cp /boot/vmlinuz* /boot/vmlinuz
 cp /boot/initramfs* /boot/initramfs.img
 cp /boot/System* /boot/System.map
