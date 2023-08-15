bfb5f4f1f03fc88bfbc01dc4c65714aa3744eb74
Luna Deards <toffee.1@icloud.com>
2023-08-15 14:11:05 +0100

    fix exit for openrc recovery

--- overlay/usr/lib/dracut/modules.d/00xenia/foxmount.sh
+++ overlay/usr/lib/dracut/modules.d/00xenia/foxmount.sh
@@ -31,34 +31,6 @@ recovery() {
     cd /sysroot/.recovery && for d in */ ; do      
         mount -t overlay overlay -o lowerdir=/sysroot/.recovery/$d:/sysroot/$d /sysroot/$d
     done
-
-}
-
-echo "foxmount: Checking for recovery"
-for p in $(getargs recovery=); do
-    if [ $p = "true" ]; then
-        recovery
-        exit
-    fi
-done
-
-echo "foxmount: Checking for config"
-if [ -f "/sysroot/roots/foxmount.sh" ]; then
-    echo "foxmount: Running config"
-    source /sysroot/roots/foxmount.sh
-    exit
-fi
-
-
-if [[ $(get_filesystem OVERLAY) != "" ]]
-then
-    echo "foxmount: Overlays on label, traditional FS layout"
-
-    cd /sysroot/.recovery && for d in */ ; do
-        mkdir -p /sysroot/roots/.recovery/.$d
-        mkdir -p /sysroot/roots/.recovery/.w_$d
-        mount -t overlay overlay -o lowerdir=/sysroot/.recovery/$d:/sysroot/$d,upperdir=/sysroot/roots/.recovery/.$d,workdir=/sysroot/roots/.recovery/.w_$d /sysroot/$d
-    done
 }
 
 foxmount() {
@@ -88,12 +60,9 @@ foxmount() {
         echo "foxmount: Mounting home"
         mount -L HOME /sysroot/home
 
-    # cow: please help mount ZFS?
-else
-    echo "foxmount: FATAL: could not find overlays!"
-    recovery
-    exit
-fi
+    elif [[ $(get_filesystem ROOTS) = "btrfs" ]]
+    then
+        echo "foxmount: btrfs found"
 
         echo "foxmount: Mounting overlay subvolume"
         mount -L ROOTS -o subvol=overlay /sysroot/overlay
@@ -101,15 +70,45 @@ fi
         echo "foxmount: Mounting home"
         mount -L ROOTS -o subvol=home /sysroot/home
 
-echo "foxmount: Checking for foxsnapshot revert"
-if [ -s /sysroot/roots/.revert ]; then
-    btrfs subvolume delete /sysroot/overlay/usr
-    btrfs subvolume snapshot /sysroot/roots/.foxsnapshot/$(cat /sysroot/roots/.revert) /sysroot/overlay/usr
-    rm /sysroot/roots/.revert
-fi
+        echo "foxmount: Setting overlay paths"
+        etc_path="/sysroot/overlay/etc"
+        var_path="/sysroot/overlay/var"
+        usr_path="/sysroot/overlay/usr"
 
-echo "foxmount: Mounting overlays on /sysroot"
-mount -t overlay overlay -o lowerdir=/sysroot/usr,upperdir=${usr_path}/usr,workdir=${usr_path}/usrw,ro /sysroot/usr
-mount -t overlay overlay -o lowerdir=/sysroot/etc,upperdir=${etc_path}/etc,workdir=${etc_path}/etcw,rw /sysroot/etc
-mount -t overlay overlay -o lowerdir=/sysroot/var,upperdir=${var_path}/var,workdir=${var_path}/varw,rw /sysroot/var
-echo "foxmount: Finished mounting overlays"
+    elif [[ $(get_filesystem XENIA) = "zfs_member" ]] # this won't work, no label on ZFS - find another way
+    then
+        echo "foxmount: zfs found"
+        /sbin/modprobe zfs
+
+        # cow: please help mount ZFS?
+    else
+        echo "foxmount: FATAL: could not find overlays!"
+        recovery
+        return 0
+    fi
+
+    echo "foxmount: Creating overlay directories if they don't exist"
+    [ ! -d "${etc_path}/etc" ] && mkdir ${etc_path}/etc
+    [ ! -d "${var_path}/var" ] && mkdir ${var_path}/var
+    [ ! -d "${usr_path}/usr" ] && mkdir ${usr_path}/usr
+
+    echo "foxmount: Creating overlay work directories if they don't exist"
+    [ ! -d "${etc_path}/etcw" ] && mkdir ${etc_path}/etcw
+    [ ! -d "${var_path}/varw" ] && mkdir ${var_path}/varw
+    [ ! -d "${usr_path}/usrw" ] && mkdir ${usr_path}/usrw
+
+    echo "foxmount: Checking for foxsnapshot revert"
+    if [ -s /sysroot/roots/.revert ]; then
+        btrfs subvolume delete /sysroot/overlay/usr
+        btrfs subvolume snapshot /sysroot/roots/.foxsnapshot/$(cat /sysroot/roots/.revert) /sysroot/overlay/usr
+        rm /sysroot/roots/.revert
+    fi
+
+    echo "foxmount: Mounting overlays on /sysroot"
+    mount -t overlay overlay -o lowerdir=/sysroot/usr,upperdir=${usr_path}/usr,workdir=${usr_path}/usrw,ro /sysroot/usr
+    mount -t overlay overlay -o lowerdir=/sysroot/etc,upperdir=${etc_path}/etc,workdir=${etc_path}/etcw,rw /sysroot/etc
+    mount -t overlay overlay -o lowerdir=/sysroot/var,upperdir=${var_path}/var,workdir=${var_path}/varw,rw /sysroot/var
+    echo "foxmount: Finished mounting overlays"
+}
+
+foxmount
